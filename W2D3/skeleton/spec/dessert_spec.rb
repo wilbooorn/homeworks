ba23910674a199c73c5566fc776fb530102ce368
Robin <robinwilborn@Robins-MacBook-Pro-2.local>
2017-04-05 07:33:12 -0700

    practice with rspec

---
+++ W2D3/skeleton/spec/dessert_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  subject(:dessert) { Dessert.new("cake", 2, chef) }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("cake")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(2)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "3", chef) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("flour")
+      expect(dessert.ingredients).to include("flour")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient("flour")
+      expect(dessert.ingredients.shuffle).to include("flour")
+    end
+  end
+
+  describe "#eat" do
+    let(:cake) { Dessert.new("cake", 1, chef) }
+    it "subtracts an amount from the quantity" do
+      cake.eat(1)
+      expect(cake.quantity).to eq(0)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { cake.eat(3) }.to raise_error("not enough left!")
+    end
+
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize)
+      expect do
+        dessert.serve
+      end.to_not raise_exception
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
+  end
+end
