51f1534c02a7823a47ed557c6c28b2b303a8c995
Robin <robinwilborn@Robins-MacBook-Pro-2.local>
2017-04-11 22:33:14 -0700

    Rails HW with house and person models and migrations

--- W3D2/plays_demo/plays.rb
+++ W3D2/plays_demo/plays.rb
@@ -60,73 +60,73 @@ class Play
     SQL
   end
 end
-
-class PlaywrightDBConnection < SQLite3::Database
-  include Singleton
-
-  def initialize
-    super('plays.db')
-    self.type_translation = true
-    self.results_as_hash = true
-  end
-end
-
-
-class Playwright
-  attr_accessor :name, :birth_year
-
-  def self.all
-    data = PlaywrightDBConnection.instance.execute("SELECT * FROM playwrights")
-    data.map { |datum| Playwright.new(datum) }
-  end
-
-  def self.find_by_name(name)
-    data = PlaywrightDBConnection.instance.execute("SELECT * FROM playwrights WHERE name = #{name}")
-    return nil unless data.length > 0
-    data.map { |datum| Playwright.new(datum) }
-  end
-
-  def initialize(options)
-    @id = options['id']
-    @name = options['name']
-    @birth_year = options['birth_year']
-  end
-
-  def create
-    raise "#{self} already in DB" if @id
-    PlaywrightDBConnection.instance.execute(<<-SQL, @name, @birth_year)
-      INSERT INTO
-        playwrights (name, birth_year)
-      VALUES
-        (?, ?)
-    SQL
-    @id = PlaywrightDBConnection.instance.last_insert_row_id
-  end
-
-
-  def update
-    raise "#{self} not in DB" unless @id
-    PlaywrightDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
-      UPDATE
-        playwrights
-      SET
-        name = ?, birth_year = ?
-      WHERE
-        id = ?
-    SQL
-  end
-
-  def get_plays
-    plays = PlaywrightDBConnection.instance.execute(<<-SQL, @id)
-      SELECT
-        *
-      FROM
-        plays
-      WHERE
-        playwright_id = ?
-    SQL
-    plays.map { |play| Play.new(play) }
-  end
-
-
-end
+#
+# class PlaywrightDBConnection < SQLite3::Database
+#   include Singleton
+#
+#   def initialize
+#     super('plays.db')
+#     self.type_translation = true
+#     self.results_as_hash = true
+#   end
+# end
+#
+#
+# class Playwright
+#   attr_accessor :name, :birth_year
+#
+#   def self.all
+#     data = PlaywrightDBConnection.instance.execute("SELECT * FROM playwrights")
+#     data.map { |datum| Playwright.new(datum) }
+#   end
+#
+#   def self.find_by_name(name)
+#     data = PlaywrightDBConnection.instance.execute("SELECT * FROM playwrights WHERE name = #{name}")
+#     return nil unless data.length > 0
+#     data.map { |datum| Playwright.new(datum) }
+#   end
+#
+#   def initialize(options)
+#     @id = options['id']
+#     @name = options['name']
+#     @birth_year = options['birth_year']
+#   end
+#
+#   def create
+#     raise "#{self} already in DB" if @id
+#     PlaywrightDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+#       INSERT INTO
+#         playwrights (name, birth_year)
+#       VALUES
+#         (?, ?)
+#     SQL
+#     @id = PlaywrightDBConnection.instance.last_insert_row_id
+#   end
+#
+#
+#   def update
+#     raise "#{self} not in DB" unless @id
+#     PlaywrightDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+#       UPDATE
+#         playwrights
+#       SET
+#         name = ?, birth_year = ?
+#       WHERE
+#         id = ?
+#     SQL
+#   end
+#
+#   def get_plays
+#     plays = PlaywrightDBConnection.instance.execute(<<-SQL, @id)
+#       SELECT
+#         *
+#       FROM
+#         plays
+#       WHERE
+#         playwright_id = ?
+#     SQL
+#     plays.map { |play| Play.new(play) }
+#   end
+#
+#
+# end
