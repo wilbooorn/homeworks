9a42736613309b8d48eae219f83f7f65872a9d41
Robin <robinwilborn@Robins-MacBook-Pro-2.local>
2017-05-08 22:01:21 -0700

    middleware practice

---
+++ W7/W7D2/skeleton/frontend/store/store.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux';
+import RootReducer from '../reducers/root_reducer';
+import { applyMiddleware } from 'redux';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch)
+  );
+
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+};
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  console.log(store.getState());
+  return next(action);
+};
+
+export default configureStore;
